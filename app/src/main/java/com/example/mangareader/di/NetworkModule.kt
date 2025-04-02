9fc392e80fa7467207b62ba29fc7c63206b06083
Jaiilioh23 <jaiihooke36@gmail.com>
2025-04-02 18:01:43 -0500

    Add app/src/main/java/com/example/mangareader/di/NetworkModule.kt 1743634902598

---
+++ app/src/main/java/com/example/mangareader/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.example.mangareader.di
+
+import com.example.mangareader.data.remote.KitsuApi
+import com.example.mangareader.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://kitsu.io/api/edge/"
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(
+                HttpLoggingInterceptor().apply {
+                    level = if (BuildConfig.DEBUG) {
+                        HttpLoggingInterceptor.Level.BODY
+                    } else {
+                        HttpLoggingInterceptor.Level.NONE
+                    }
+                }
+            )
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideKitsuApi(retrofit: Retrofit): KitsuApi {
+        return retrofit.create(KitsuApi::class.java)
+    }
+}
